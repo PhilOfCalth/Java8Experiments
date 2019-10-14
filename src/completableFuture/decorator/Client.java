1cfaf5c14fce3643b0b05b86c3798da41b1a4a3b
PhilOfCalth <philiplarkin8383@gmail.com>
2019-10-14 20:31:20 +0100

    Client just measures what we care about

--- src/completableFuture/decorator/Client.java
+++ src/completableFuture/decorator/Client.java
@@ -1,18 +1,20 @@
 package completableFuture.decorator;
 
+import java.util.Date;
+
 public class Client {
 
     public static void main( String[] args ) {
-        TextComponent textComponent = new TextComponent("Hello World!\n" +
-                                                        "This is Phil, playing with his code.\n" +
-                                                        "Goodbye now!");
-        System.out.println("Origional:\n"+textComponent.draw()+"\n");
 
-        VisualComponentInterface linedComponent = new LineNumberDecorator(textComponent);
-        System.out.println("With Lines:\n"+linedComponent.draw()+"\n");
-
-        VisualComponentInterface borderedComponent = new BorderDecorator(linedComponent);
+        //org.apache.commons.lang3.time.StopWatch, if I was bothered with dependencies
+        long startTime = (new Date()).getTime();
+        VisualComponentInterface borderedComponent = new BorderDecorator(
+                                                        new LineNumberDecorator(
+                                                                new TextComponent("Hello World!\n" +
+                                                                            "This is Phil, playing with his code.\n" +
+                                                                            "Goodbye now!")));
         System.out.println("With Borders:\n"+borderedComponent.draw()+"\n");
+        System.out.println("That took "+Long.toString((new Date()).getTime() - startTime)+"ms");
 
     }
 }
