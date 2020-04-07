b06bfdf0eafa3a074b4e4aec41a098db4e1189da
PhilOfCalth <philiplarkin8383@gmail.com>
2020-04-07 22:40:45 +0100

    parallell stream

--- src/stream/visitor/Client.java
+++ src/stream/visitor/Client.java
@@ -9,6 +9,7 @@ import stream.visitor.pricingVisitors.ShoppingCartVisitor;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Client {
     public static void main(String[] args){
@@ -19,14 +20,13 @@ public class Client {
         shoppingCart.add(new BiddableItemElement(3, 2100));
 
         ShoppingCartVisitor visitor = new PricingVisitor();
-        int total = 0;
 
         //org.apache.commons.lang3.time.StopWatch, if I was bothered with dependencies
         final long startTime = (new Date()).getTime();
 
-        for(ShoppingCartElement element : shoppingCart){
-            total += element.accept(visitor);
-        }
+        int total = shoppingCart.parallelStream()
+                        .map(element -> element.accept(visitor))
+                        .collect(Collectors.summingInt(Integer::intValue));
 
         System.out.println("That took "+Long.toString((new Date()).getTime() - startTime)+"ms");
         System.out.println("The total billble for that cart is "+total);
