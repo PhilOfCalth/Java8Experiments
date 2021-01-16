3656895c9df983b6c370592b2f59307f22645735
PhilLarkin <philip.larkin@naimuri.com>
2021-01-16 12:23:01 +0000

    fixed repeated type

--- src/doubleDispatch/java/ImperialShip.java
+++ src/doubleDispatch/java/ImperialShip.java
@@ -2,10 +2,10 @@ package doubleDispatch.java;
 
 public class ImperialShip {
     public void fireUpon(ImperialShip enemy){
-        System.out.println("Apologies captain, but I do not know the specifies of this vessel, nor the enemy!");
+        System.out.println("Apologies captain, but I do not know the specifics of this vessel, nor the enemy!");
     }
 
     public void fireUpon(DauntlessCruiser enemy){
-        System.out.println("I see the Dauntless, but I do not know the specifies of our vessel!");
+        System.out.println("I see the Dauntless, but I do not know the specifics of this vessel!");
     }
 }
